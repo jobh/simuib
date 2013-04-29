9bcb3d7dc47c52bf17155132a277adf111d53f7b
Joachim B Haga <jobh@simula.no>
2013-04-29 22:49:37 +0200

    modify notation, add schur variants

--- siam/block/biot.py
+++ siam/block/biot.py
@@ -14,7 +14,7 @@ set_log_level(PROGRESS)
 
 N=32
 mesh = UnitSquareMesh(N,N)
-dim = mesh.topology().dim()
+Nd = mesh.topology().dim()
 
 n = FacetNormal(mesh)
 
@@ -49,11 +49,11 @@ class Permeability(Expression):
             tensor[0,0] = 1e-6
             tensor[1,1] = tensor[0,0]
 
-S = Constant(0.000)
+b = Constant(1e-12)
 alpha = Constant(1.0)
 Lambda = Permeability()
 
-t_n = Constant( [0.0]*dim )
+t_n = Constant( [0.0]*Nd )
 
 dt = Constant(.02)
 T = 0.1
@@ -61,24 +61,24 @@ T = 0.1
 r = Constant(0)
 
 def sigma(v):
-    return 2.0*mu*sym(grad(v)) + lmbda*tr(grad(v))*Identity(dim)
+    return 2.0*mu*sym(grad(v)) + lmbda*tr(grad(v))*Identity(Nd)
 def v_D(q):
     return -Lambda*grad(q)
-def b(w,r):
+def coupling(w,r):
     return - alpha * r * div(w)
 
 a00 = inner(grad(omega), sigma(v)) * dx
-a01 = b(omega,q) * dx
-a10 = b(v,phi) * dx
-a11 = -(S*phi*q - dt*inner(grad(phi),v_D(q))) * dx
+a01 = coupling(omega,q) * dx
+a10 = coupling(v,phi) * dx
+a11 = -(b*phi*q - dt*inner(grad(phi),v_D(q))) * dx
 
 L0 = dot(t_n, omega) * ds
-L1 = b(u,phi) * dx - (r*dt + S*p)*phi * dx
+L1 = coupling(u,phi) * dx - (r*dt + b*p)*phi * dx
 
 # Create boundary conditions.
 
-bc_u_bedrock        = DirichletBC(V, [0.0]*dim, lambda x,_: near(x[1],0))
-bc_p_drained_edges  = DirichletBC(Q,  0.0,      lambda x,_: near(x[1],1))
+bc_u_bedrock        = DirichletBC(V, [0.0]*Nd, lambda x,_: near(x[1],0))
+bc_p_drained_edges  = DirichletBC(Q,  0.0,     lambda x,_: near(x[1],1))
 
 bcs = [bc_u_bedrock, bc_p_drained_edges]
 
@@ -87,68 +87,88 @@ bcs = [bc_u_bedrock, bc_p_drained_edges]
 AA, AAns = block_symmetric_assemble([[a00,a10],[a01,a11]], bcs=bcs)
 bb = block_assemble([L0,L1], bcs=bcs, symmetric_mod=AAns)
 
-[[A, B],
- [C, D]] = AA
+[[A,  B],
+ [BT, C]] = AA
 
 def exact_schur():
     Ai = AmesosSolver(A)
-    Sp = ML(collapse(D-C*InvDiag(A)*B))
-    Si = LGMRES(D-C*Ai*B, precond=Sp, iter=1)
+    Sp = ML(collapse(C-BT*InvDiag(A)*B))
+    Si = LGMRES(C-BT*Ai*B, precond=Sp, iter=1)
 
     SS = [[Ai, B],
-          [C,  Si]]
+          [BT, Si]]
     return block_mat(SS).scheme('sgs')
+
+def inexact_schur():
+    Ai = ML(A)
+    Sp = ML(collapse(C-BT*InvDiag(A)*B))
+    SS = [[Ai, B],
+          [BT, Sp]]
+    return block_mat(SS).scheme('tgs')
+
+def exact_A_inexact_schur():
+    Ai = AmesosSolver(A)
+    Sp = ML(collapse(C-BT*InvDiag(A)*B))
+    SS = [[Ai, B],
+          [BT, Sp]]
+    return block_mat(SS).scheme('tgs')
 
 def drained_split():
     Ai = AmesosSolver(A)
-    Di = AmesosSolver(D)
+    Ci = AmesosSolver(C)
     SS = [[Ai, B],
-          [C, Di]]
+          [BT, Ci]]
     return block_mat(SS).scheme('tgs')
 
 def undrained_split():
-    SA = collapse(A-float(1/S)*B*C)
+    SA  = collapse(A-1/float(b)*B*BT)
     SAi = AmesosSolver(SA)
-    Di = AmesosSolver(D)
-    SS = [[SAi, B],
-          [C,  Di]]
+    Ci  = AmesosSolver(C)
+    SS  = [[SAi, B],
+           [BT,  Ci]]
     return block_mat(SS).scheme('tgs')
 
 def fixed_strain():
     Ai = AmesosSolver(A)
-    Di = AmesosSolver(D)
+    Ci = AmesosSolver(C)
     SS = [[Ai, B],
-          [C, Di]]
+          [BT, Ci]]
     return block_mat(SS).scheme('tgs', reverse=True)
 
-    #SS1 = [[Ai, 0], [0, Di]]
-    #SS2 = [[1, -B*Di], [0, 1]]
+    #SS1 = [[Ai, 0], [0, Ci]]
+    #SS2 = [[1, -B*Ci], [0, 1]]
     #return block_mat(SS1)*block_mat(SS2)
 
 def fixed_stress():
-    beta = 2*mu + dim*lmbda
-    SD = collapse(D+dim*float(1/beta))
-    SDi = AmesosSolver(SD)
-    Ai = AmesosSolver(A)
-    SS = [[Ai, B],
-          [C, SDi]]
+    beta = 2*mu + Nd*lmbda
+    SC   = collapse(C+Nd/float(beta))
+    SCi  = AmesosSolver(SC)
+    Ai   = AmesosSolver(A)
+    SS   = [[Ai, B],
+            [BT, SCi]]
     return block_mat(SS).scheme('tgs', reverse=True)
 
+x0 = AA.create_vec()
+x0.randomize()
+
 def run(prec, runs=[0]):
-    AAinv = LGMRES(AA, precond=eval(prec)())
-
-    x0 = AA.create_vec()
-    x0.randomize()
-
-    xx = AAinv(initial_guess=x0, maxiter=20, tolerance=1e-10, show=2)*bb
-    pyplot.semilogy(AAinv.residuals, marker='o', color='bgrkcmy'[runs[0]], label=prec)
+    from time import time
+    try:
+        before = time()
+        AAinv = LGMRES(AA, precond=eval(prec)())
+        xx = AAinv(initial_guess=x0, maxiter=20, tolerance=1e-10, show=2)*bb
+        pyplot.semilogy(AAinv.residuals, marker='o', color='bgrkcmy'[runs[0]], label='%s (%.1f)'%(prec,time()-before))
+    except Exception, e:
+        print prec, e
     runs[0] += 1
 
 run('drained_split')
-#run('undrained_split')
+run('undrained_split')
 run('fixed_stress')
 run('fixed_strain')
 run('exact_schur')
+run('inexact_schur')
+run('exact_A_inexact_schur')
 
 pyplot.legend()
 pyplot.show()
