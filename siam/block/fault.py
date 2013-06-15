13dd678031453409c6d9570250cd1c8ef36399ef
Joachim B Haga <jobh@simula.no>
2013-06-15 11:59:56 +0200

    Revert "Use symmetric BCs on fault"
    This reverts commit 72a40cead971186cb0a3484c6f90800a82f1ac71.
    
    Conflicts:
    	siam/block/fault.py

--- siam/block/fault.py
+++ siam/block/fault.py
@@ -5,8 +5,10 @@ from block import *
 
 # Function spaces, elements
 
-N  = int(cl_args.get('N', 10)) # should be even
-mesh = RectangleMesh(-1, -1, 1, 0, N, N//2)
+eps = 1e-3
+r = Rectangle(-1, -1, 1, 1)
+c = Rectangle(-1, -eps, 0, eps)
+mesh = Mesh(r-c, 200)
 
 Nd = mesh.topology().dim()
 x = mesh.ufl_cell().x
@@ -55,13 +57,11 @@ L1 = coupling(u,phi) * dx - (r*dt + b*p)*phi * dx
 
 # Create boundary conditions.
 
-bc_u_pt = DirichletBC(V.sub(0), 0.0, lambda x,bdry: near(x[0],1.0) and near(x[1],0.0), method='pointwise')
-bc_u = DirichletBC(V, [0.0]*Nd,  lambda x,bdry: near(x[0], 1.0))
-bc_u_mirror = DirichletBC(V.sub(1), 0.0, lambda x,bdry: near(x[1],0.0) and x[0] >= 0)
-bc_p = DirichletBC(Q, -x[0],     lambda x,bdry: bdry and not near(x[1],0.0))
-bc_p_fault = DirichletBC(Q, 1.0, lambda x,bdry: near(x[1],0.0) and x[0] <= 0.0, method='pointwise')
+bc_u = DirichletBC(V, [0.0]*Nd,  lambda x,bdry: near(x[-1], 1.0))
+bc_p = DirichletBC(Q, -x[0],     lambda x,bdry: bdry)
+bc_p_fault = DirichletBC(Q, 1.0, lambda x,bdry: abs(x[-1]) <= 2*eps and x[0] <= 0.0, method='pointwise')
 
-bcs = [[bc_u_pt, bc_u_mirror], [bc_p, bc_p_fault]]
+bcs = [bc_u, [bc_p, bc_p_fault]]
 
 # Assemble the matrices and vectors
 
